677333d2607dffb4880b555b206a73e6a34e5787
ShravaniMishra <shravniguddi@gmail.com>
2023-03-13 02:27:49 +0530

    Contact Page added

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -2,8 +2,29 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
+    <>
+      <div style={{ marginTop: "100px" }}>
+        <img
+          src="https://media.tenor.com/Mn3pGltPKc4AAAAM/run.gif"
+          alt="govind"
+        />
+
+        <img
+          style={{ height: "300px" }}
+          src="https://i.natgeofe.com/k/7bfcf2d2-542e-44f0-962a-c36f2efa98a5/heart.jpg"
+          alt="love"
+        />
+
+        <img
+          style={{ height: "300px" }}
+          src="https://media.tenor.com/_iSX87na8aQAAAAi/woman-running-joypixels.gif"
+          alt="shravani"
+        />
+      </div>
+
+      <h1 className="text-danger">Masai Connects Together</h1>
+    </>
+  );
 }
 
 export default Contact
