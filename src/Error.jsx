ee229e8aefac2f3b1c78ca116059f39e8bcbd85e
ShravaniMishra <shravniguddi@gmail.com>
2023-03-12 21:47:56 +0530

    Added styling to Error Page

--- src/Error.jsx
+++ src/Error.jsx
@@ -1,13 +1,50 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
 
 const Error = () => {
   return (
-    <>
-    <h1>404 !! 😭 Page Not Found</h1>
-    <NavLink to="/">Go Back</NavLink>
-    </>
-  )
-}
+    <Wrapper>
+      <h1>404 !! 😭 Page Not Found</h1>
+      <NavLink to="/">Go Back</NavLink>
+    </Wrapper>
+  );
+};
+const Wrapper = styled.div`
+  width: 100%;
+  height: 80vh;
+  background-color: #ecf5f5;
 
-export default Error
+  h1 {
+    padding-top: 10%;
+    color: red;
+    overflow-y: hidden;
+  }
+  a {
+    text-decoration: none;
+    color: black;
+    font-size: 30px;
+    font-weight: bold;
+    position: relative;
+  }
+  a::after {
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    content: "";
+    width: 100%;
+    border: 2px solid red;
+    transition: all 0.5s ease-in-out;
+    transform: scale(0);
+    transform-origin: left;
+  }
+  a:hover::after {
+    transform: scale(1);
+  }
+  /* a:hover {
+    color: red;
+    text-decoration: underline;
+  } */
+`;
+
+export default Error;
