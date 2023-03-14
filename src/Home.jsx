f9849b227ebdc8ef6e83805756ea7cf31f96420f
ShravaniMishra <shravniguddi@gmail.com>
2023-03-15 02:50:41 +0530

    added some work

--- src/Home.jsx
+++ src/Home.jsx
@@ -8,38 +8,58 @@ const Home = () => {
       setTime(newTime);
     };
     setInterval(handleClick, 1000);
+
+    let [love, setLove] = useState("");
+    const Change = ()=>{
+      setLove('I Love You Govind')
+    }
   return (
     <>
-      <div className="Home_main">
-        <h1
-          style={{
-            color: "white",
-            fontSize: "40px",
-            textAlign:"center"
-          }}
-        >
-          Welcome to my website
-        </h1>
-        <h3 style={{ fontSize: "25px",color:"white" ,textAlign:"right" ,marginRight:"10px",marginTop:"-55px"}}>{time}</h3>
+      <div style={{backgroundImage: "linear-gradient(rgb(52, 94, 94),rgb(202, 185, 202))"}}>
+        <div className="Home_main">
+          <h1
+            style={{
+              color: "white",
+              fontSize: "40px",
+              textAlign: "center",
+              marginBottom: "20px",
+            }}
+          >
+            Welcome to My Website
+          </h1>
+          <h3
+            style={{
+              fontSize: "25px",
+              color: "white",
+              textAlign: "right",
+              marginRight: "10px",
+              marginTop: "-55px",
+            }}
+          >
+            {time}
+          </h3>
+        </div>
         <div className="Home">
           <img
-            style={{
-              width: "40%",
-              borderRadius: "11px",
-              transition: "all 5.2s ease-in-out",
-            }}
             src="https://i.ibb.co/5K0cjj6/Whats-App-Image-2023-03-04-at-8-30-28-PM.jpg"
             alt="Shravani"
           />
-          <div>
-            <h2>
-              Hey It's me{" "}
-              <span style={{ textDecoration: "bold", color: "brown" }}>
-                Queen of Govind.
-              </span>
-            </h2>
-            <p></p>
-          </div>
+        </div>
+        <div className="box">
+          <h2>
+            Hey It's me {" "}
+            <span style={{ textDecoration: "bold", color: "brown" }} >
+              Queen of Govind.
+            </span>
+          </h2>
+          <button
+            className="btn btn-outline-success"
+            onClick={Change}
+            style={{ margin: "auto", marginLeft: "-25%" }}
+          >
+            Click Me
+          </button>
+          <h1 className="text-danger">{love}</h1>
         </div>
       </div>
     </>
